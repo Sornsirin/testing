d457e2b5b42a70797babc4ba63ac47a49f53a7fb
Sornsirin <so11.samart@gmail.com>
2015-02-18 14:14:13 +0700

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,6 +1,23 @@
-public class Testing{
-    public static void main(String args[]){
-        System.out.println("Hello World");
-    }
+import javax.swing.JOptionPane;   
+public class Multiply{
+	public static void main(String[] args){
+		String firstNum, secondNum, answer;
+		int num1, num2, sum;
+
+		//read user numbers
+		firstNum = JOptionPane.showInputDialog("Enter First Integer:");
+		secondNum = JOptionPane.showInputDialog("Enter Second Integer:");
+
+		//convert numbers
+		num1 = Integer.parseInt( firstNum );
+		num2 = Integer.parseInt( secondNum );
+
+		//calculate adder
+		sum = num1 * num2;
+
+		answer = num1 + " x " + num2 + " = " + sum;
+
+		//Display results
+		JOptionPane.showMessageDialog(null, answer, "Answer Adder ", JOptionPane.INFORMATION_MESSAGE);
+	}
 }
-   
